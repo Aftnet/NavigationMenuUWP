e50e1f5dede5980f74d4f083d1ecbcf673cd7616
Alberto Fustinoni <alberto-fustinoni@outlook.com>
2016-06-17 16:37:03 +0900

    Added sample app

---
+++ SampleApp/Navigation/NavigationItems.cs
@@ -0,0 +1,24 @@
+﻿using NavigationMenuUWP;
+using SampleApp.Pages;
+using Windows.UI.Xaml.Controls;
+
+namespace SampleApp.Navigation
+{
+    class NavigationItems
+    {
+        private static readonly NavMenuItem[] StaticItemsTop = new NavMenuItem[]
+        {
+            new NavMenuItem { Symbol = Symbol.AllApps, Label = "Main page", DestPage = typeof(MainPage) },
+            new NavMenuItem { Symbol = Symbol.Favorite, Label = "Other page", DestPage = typeof(OtherPage) },
+        };
+
+        private static readonly NavMenuItem[] StaticItemsBottom = new NavMenuItem[]
+        {
+            new NavMenuItem { Symbol = Symbol.Setting, Label = "Settings", DestPage = typeof(SettingsPage) },
+        };
+
+        public NavMenuItem[] TopItems { get { return StaticItemsTop; } }
+
+        public NavMenuItem[] BottomItems { get { return StaticItemsBottom; } }
+    }
+}
