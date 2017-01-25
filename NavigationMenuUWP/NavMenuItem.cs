6b6458bbb57833e9f1337e1ba611ada73d1f6ac6
Alberto Fustinoni <alberto@aftnet.net>
2017-01-25 22:08:45 +0900

    Use strings instead of glyph enumeration to have a lot mre options for icons

--- NavigationMenuUWP/NavMenuItem.cs
+++ NavigationMenuUWP/NavMenuItem.cs
@@ -9,14 +9,7 @@ namespace NavigationMenuUWP
     public sealed class NavMenuItem
     {
         public string Label { get; set; }
-        public Symbol Symbol { get; set; }
-        public char SymbolAsChar
-        {
-            get
-            {
-                return (char)this.Symbol;
-            }
-        }
+        public string Symbol { get; set; }
 
         public Type DestPage { get; set; }
         public object Arguments { get; set; }
--- SampleApp/Navigation/NavigationItems.cs
+++ SampleApp/Navigation/NavigationItems.cs
@@ -1,6 +1,5 @@
 ﻿using NavigationMenuUWP;
 using SampleApp.Pages;
-using Windows.UI.Xaml.Controls;
 
 namespace SampleApp.Navigation
 {
@@ -8,13 +7,13 @@ namespace SampleApp.Navigation
     {
         private static readonly NavMenuItem[] StaticItemsTop = new NavMenuItem[]
         {
-            new NavMenuItem { Symbol = Symbol.AllApps, Label = "Main page", DestPage = typeof(MainPage) },
-            new NavMenuItem { Symbol = Symbol.Favorite, Label = "Other page", DestPage = typeof(OtherPage) },
+            new NavMenuItem { Symbol = "\ue10f", Label = "Main page", DestPage = typeof(MainPage) },
+            new NavMenuItem { Symbol = "\ue12b", Label = "Other page", DestPage = typeof(OtherPage) },
         };
 
         private static readonly NavMenuItem[] StaticItemsBottom = new NavMenuItem[]
         {
-            new NavMenuItem { Symbol = Symbol.Setting, Label = "Settings", DestPage = typeof(SettingsPage) },
+            new NavMenuItem { Symbol = "\ue115", Label = "Settings", DestPage = typeof(SettingsPage) },
         };
 
         public NavMenuItem[] TopItems { get { return StaticItemsTop; } }
