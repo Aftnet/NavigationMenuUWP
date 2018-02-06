8b7cc15d0648bbe0d55e35a3b0f916e4da38f9dc
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-02-06 16:49:53 +0900

    Logic

---
+++ SampleApp/AppResources.xaml.cs
@@ -0,0 +1,28 @@
+﻿using NavigationMenuUWP;
+using SampleApp.Pages;
+using Windows.UI.Xaml;
+
+namespace SampleApp
+{
+    public sealed partial class AppResources : ResourceDictionary
+    {
+        private readonly NavMenuItem[] TopNavItems = new NavMenuItem[]
+        {
+            new NavMenuItem { Symbol = "\ue10f", Label = "Main page", Param = typeof(MainPage) },
+            new NavMenuItem { Symbol = "\ue12b", Label = "Other page", Param = typeof(OtherPage) },
+        };
+
+        private readonly NavMenuItem[] BottomNavItems = new NavMenuItem[]
+        {
+            new NavMenuItem { Symbol = "\ue115", Label = "Settings", Param = typeof(SettingsPage) },
+        };
+
+        public AppResources()
+        {
+            this.InitializeComponent();
+
+            this.Add(nameof(TopNavItems), TopNavItems);
+            this.Add(nameof(BottomNavItems), BottomNavItems);
+        }
+    }
+}
